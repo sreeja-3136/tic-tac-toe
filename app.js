fd523ccb9c055d8d85e583c53bddf70efe3e2705
sreeja46 <sreejadevireddy6@gmail.com>
2025-07-10 20:15:46 +0530

    minor changes

--- app.js
+++ app.js
@@ -54,9 +54,9 @@ const showwinner=(winner)=>{
 }
 const checkwinner = () =>{
     for(let pattern of winpatterns){
-        let pos1val=boxes.pattern[0].innerText;
-        let pos2val=boxes.pattern[1].innerText;
-        let pos3val=boxes.pattern[2].innerText;
+        let pos1val=boxes[pattern[0]].innerText;
+        let pos2val=boxes[pattern[1]].innerText;
+        let pos3val=boxes[pattern[2]].innerText;
 
         if(pos1val !="" && pos2val !="" && pos3val !=""){
             if(pos1val===pos2val && pos2val===pos3val){
